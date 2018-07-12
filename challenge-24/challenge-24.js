5736a142eb93e1efcf649e374dec7f7dbc6f0186
Rachel <rachelcurioso@gmail.com>
2018-07-12 07:16:57 -0300

    changed files to my own exercise

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -1,75 +1,112 @@
-/*
-Nossa calculadora agora está funcional! A ideia desse desafio é modularizar
-o código, conforme vimos na aula anterior. Quebrar as responsabilidades
-em funções, onde cada função faça somente uma única coisa, e faça bem feito.
+(function(win, doc){
+  'use strict'
+  /*
+  Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+  As regras são:
 
-- Remova as duplicações de código;
-- agrupe os códigos que estão soltos em funções (declarações de variáveis,
-listeners de eventos, etc);
-- faça refactories para melhorar esse código, mas de forma que o mantenha com a
-mesma funcionalidade.
-*/
+  - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+  diretamente;
+  - O input deve iniciar com valor zero;
+  - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+  - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+  multiplicação(x) e divisão(÷);
+  - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+  que irá limpar o input, deixando-o com valor 0;
 
-var $visor = document.querySelector('[data-js="visor"]');
-var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
-var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]');
-var $buttonCE = document.querySelector('[data-js="button-ce"]');
-var $buttonEqual = document.querySelector('[data-js="button-equal"]');
+  - A cada número pressionado, o input deve atualizar concatenando cada valor
+  digitado, como em uma calculadora real;
+  - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+  operação no input. Se o último caractere no input já for um símbolo de alguma
+  operação, esse caractere deve ser substituído pelo último pressionado.
+  Exemplo:
+  - Se o input tem os valores: "1+2+", e for pressionado o botão de
+  multiplicação (x), então no input deve aparecer "1+2x".
+  - Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+  input;
+  - Ao pressionar o botão "CE", o input deve ficar zerado.
+  */
 
-Array.prototype.forEach.call($buttonsNumbers, function(button) {
-  button.addEventListener('click', handleClickNumber, false);
-});
-Array.prototype.forEach.call($buttonsOperations, function(button) {
-  button.addEventListener('click', handleClickOperation, false);
-});
-$buttonCE.addEventListener('click', handleClickCE, false);
-$buttonEqual.addEventListener('click', handleClickEqual, false);
+  /*
+  - Separar numeros de operadores
+  - tirar função dentro de função dos numeros
+  - Separar função de printar numero pra função de printar operadores
+  - Criar função para verificar se
+  */
 
-function handleClickNumber() {
-  $visor.value += this.value;
-}
+  var $display = doc.querySelector('[data-js="values"]')
+  var $numberBtn = doc.querySelectorAll('[data-js="numbers"]')
+  var $operatorBtn = doc.querySelectorAll('[data-js="operators"]')
+  var $clearBtn = doc.querySelector('[data-js="clear"]')
+  var $equalBtn = doc.querySelector('[data-js="result"]')
 
-function handleClickOperation() {
-  $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  $visor.value += this.value;
-}
+  var operationsArr = ['+', '-', 'x', '÷']
+  var lastCharRegex = new RegExp('\\D$')
 
-function handleClickCE() {
-  $visor.value = 0;
-}
+  $numberBtn.forEach(function(item){
+    item.addEventListener('click', function(){
+      printItem(item)
+    }, false)
+  })
 
-function isLastItemAnOperation(number) {
-  var operations = ['+', '-', 'x', '÷'];
-  var lastItem = number.split('').pop();
-  return operations.some(function(operator) {
-    return operator === lastItem;
-  });
-}
+  $operatorBtn.forEach(function(item){
+    item.addEventListener('click', function(e){
+      e.preventDefault
+      printItem(item)
+    }, false)
+  })
 
-function removeLastItemIfItIsAnOperator(number) {
-  if(isLastItemAnOperation(number)) {
-    return number.slice(0, -1);
+  $clearBtn.addEventListener('click', function(e){
+    e.preventDefault
+    clear($display)
+  },false)
+
+  $equalBtn.addEventListener('click', function(e){
+    handleResult($display.value)
+  }, false)
+
+  function printItem(item){
+    if (isLastItemOP($display.value) && (operationsArr.indexOf(item.value) !== -1))
+      return $display.value = $display.value.slice(0,-1) + item.value
+
+    return $display.value === '0' ? $display.value = item.value : $display.value += item.value
   }
-  return number;
-}
 
-function handleClickEqual() {
-  $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  var allValues = $visor.value.match(/\d+[+x÷-]?/g);
-  $visor.value = allValues.reduce(function(accumulated, actual) {
-    var firstValue = accumulated.slice(0, -1);
-    var operator = accumulated.split('').pop();
-    var lastValue = removeLastItemIfItIsAnOperator(actual);
-    var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
-    switch(operator) {
-      case '+':
-        return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
-      case '-':
-        return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-      case 'x':
-        return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
-      case '÷':
-        return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
-    }
-  });
-}
+  function isLastItemOP(values){
+    return lastCharRegex.test(values)
+  }
+
+  function clear($display){
+    $display.value = 0
+  }
+
+  function handleResult(values){
+    lastCharRegex.test(values) ? values = values.slice(0,-1) : ''
+
+    values = values.match(/(?:\d+)[+x÷-]?/g);
+
+    var result = values.reduce(function(acumulado, atual){
+      var op = lastCharRegex.test(acumulado) ? acumulado.split('').pop() : ''
+      var nextOp = lastCharRegex.test(atual) ? atual.split('').pop() : ''
+      var value1 = lastCharRegex.test(acumulado) ? acumulado.slice(0,-1) : acumulado
+      var value2 = lastCharRegex.test(atual) ? atual.slice(0,-1) : atual
+
+      switch (op) {
+        case '+':
+          return (Number(value1) + Number(value2)) + nextOp;
+        case '-':
+          return  (Number(value1) - Number(value2)) + nextOp;
+        case 'x':
+          return (Number(value1) * Number(value2)) + nextOp;
+        case '÷':
+          return (Number(value1) / Number(value2)) + nextOp;
+      }
+    })
+    $display.value = result
+  }
+
+  function removeLastOp(values){
+    lastCharRegex.test(values) ? values = values.slice(0,-1) : ''
+  }
+
+
+})(window, document)
